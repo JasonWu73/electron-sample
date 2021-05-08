11168952cd4fb5a317a4e62e9e874a1d3b357d2d
wuxianjie <wuxianjiezh@gmail.com>
2021-05-08 15:29:14 +0800

    简化项目文件，保留上次窗口位置，完成页面布局

--- main.js
+++ main.js
@@ -1,60 +1,51 @@
-// Modules to control application life and create native browser window
 const {app, BrowserWindow} = require('electron');
+const windowStateKeeper = require('electron-window-state');
 const path = require('path');
 
-const {ipcAskFruit, ipcAskAnswerFruit} = require('./main/dialog');
-
-// Create a new BrowserWindow when `app` is ready
 function createWindow() {
-  // Create the browser window
-  const mainWindow = new BrowserWindow({
-    width: 1000,
-    height: 800,
-    minWidth: 550,
-    minHeight: 350,
-    webPreferences: {
-      preload: path.join(__dirname, 'renderer/mainPreload.js')
-    },
-    // Showing window gracefully (use a color close to app's background)
-    backgroundColor: '#2B2E3B'
+  // electron-window-state: 1
+  const mainWindowState = windowStateKeeper({
+    defaultWidth: 500,
+    defaultHeight: 650
   });
 
-  // Load HTML into the BrowserWindow
+  // electron-window-state: 2
+  const mainWindow = new BrowserWindow({
+    x: mainWindowState.x,
+    y: mainWindowState.y,
+    width: mainWindowState.width,
+    height: mainWindowState.height,
+    minWidth: 350,
+    maxWidth: 650,
+    minHeight: 300,
+    webPreferences: {
+      preload: path.join(__dirname, 'renderer/preload.js')
+    }
+  });
+
+  // electron-window-state: 3
+  mainWindowState.manage(mainWindow);
+
   mainWindow.loadFile('renderer/main.html');
 
-  // Open DevTools - Remove for PRODUCTION!
   const openDevTools = windows =>
       windows.forEach(window => window.webContents.openDevTools());
 
   openDevTools([mainWindow]);
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
   createWindow();
 
   app.on('activate', () => {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0) {
       createWindow();
     }
   });
 });
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit();
   }
 });
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
-
-ipcAskFruit();
-ipcAskAnswerFruit()
